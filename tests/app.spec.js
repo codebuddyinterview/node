888e5cfb6e95cd19dc4e5aca4dd44add7ae815ec
suman sarkar <suman@codebuddy.co>
2022-02-11 12:18:44 +0530

    update: Test seeder - case 1

--- tests/app.spec.js
+++ tests/app.spec.js
@@ -1,16 +1,23 @@
-const request = require('supertest');
-const app = require('../src/app');
 const { connect, disconnect } = require('../src/helpers/db.helper');
+const User = require('../src/schema/user.schema');
+const Post = require('../src/schema/post.schema');
 
 beforeAll(async () => {
     await connect()
+    await User.deleteMany({});
+    await Post.deleteMany({});
 });
 afterAll(async () => await disconnect());
 
 describe('POST /', () => {
-    describe('should return 200 OK', (done) => {
-        test("true", () => {
-            expect(true).toBe(true);
-        })
+    describe('Test seeder', (done) => {
+        test("DB should be empty at first", async () => {
+            const [allUsersCount, allPostsCount] = await Promise.all([
+                User.countDocuments({}),
+                Post.countDocuments({})
+            ]);
+            expect(allUsersCount).toBe(0);
+            expect(allPostsCount).toBe(0);
+        });
     });
 })
