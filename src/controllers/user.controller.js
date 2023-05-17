094869ab0cdba4e82e857efceb398858cc74ae8b
anmol kumar <anmol.kumar@netprophetsglobal.com>
2023-05-17 16:45:51 +0530

    Pagination

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,9 +2,39 @@ const User = require("../schema/user.schema");
 
 module.exports.getUsersWithPostCount = async (req, res) => {
   try {
-    //TODO: Implement this API
+    let limit = parseInt(req?.query?.limit) || 10;
+    let page = parseInt(req?.query?.page) || 1;
+    let skip = limit * (page - 1);
+    let users = await User.aggregate([
+      { $lookup: { from: "posts", localField: "_id", foreignField: "userId", as: "R" } },
+      {
+        $project: {
+          _id: "$_id",
+          name: "$name",
+          posts: { $size: "$R" },
+        },
+      },
+    ])
+      .skip(skip)
+      .limit(limit);
+    let totalCount = await User.find({}).count();
 
-    res.status(200).json({});
+    let hasPrevPage = page > 1 ? true : false;
+    let hasNextPage = page < Math.ceil(totalCount / limit) ? true : false;
+
+    let pagination = {
+      totalDocs: totalCount,
+      limit: limit,
+      page: page,
+      totalPages: Math.ceil(totalCount / limit),
+      pagingCounter: page > 1 ? limit * (page - 1) + 1 : 1,
+      hasPrevPage: hasPrevPage,
+      hasNextPage: hasNextPage,
+      prevPage: hasPrevPage ? page - 1 : null,
+      nextPage: hasNextPage ? parseInt(page) + 1 : null,
+    };
+
+    return res.status(200).json({ data: { users, pagination } });
   } catch (error) {
     res.send({ error: error.message });
   }
