e84156f777a0c03f86a1ff53814e557254f68a27
Stormspirit03 <Raviraj03.py@gmail.com>
2023-10-21 20:08:00 +0530

    all tests cases passing.

--- src/helpers/db.helper.js
+++ src/helpers/db.helper.js
@@ -2,5 +2,6 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config()
 
-module.exports.connect = () => mongoose.connect(process.env.DB_URL);
+module.exports.connect = () => mongoose.connect('mongodb://127.0.0.1:27017/test'); //process.env.DB_URL mongodb://127.0.0.1:27017
+
 module.exports.disconnect = () => mongoose.disconnect();
